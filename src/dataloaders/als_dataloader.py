42eb7183976792b66397a49c5827f47b688477a9
ekaterinatsinovnik <ekaterinatsinovnik@gmail.com>
2024-04-12 17:42:39 +0300

    Initial commit

---
+++ src/dataloaders/als_dataloader.py
@@ -0,0 +1,31 @@
+import numpy as to_numpy
+from scipy.sparse import csr_matrix
+from sklearn.preprocessing import LabelEncoder
+
+from .base_dataloader import BaseDataloader
+
+
+class ALSLoader(BaseDataloader):
+    def make_sparse(self, grouped_ratings):
+        ratings = grouped_ratings.explode()
+
+        user_encoder = LabelEncoder()
+        user_index = user_encoder.fit_transform(ratings.index.to_numpy())
+
+        item_encoder = LabelEncoder()
+        item_index = item_encoder.fit_transform(ratings.values)
+
+        user_num = len(grouped_ratings)
+        item_num = len(np.unique(item_index))
+
+        sparse_matrix = csr_matrix(
+            (np.ones(len(user_index)), (user_index, item_index)),
+            shape=(user_num, item_num),
+        )
+
+        return sparse_matrix, user_encoder, item_encoder
+
+    def load_dataset(self):
+        train_csr, user_encoder, item_encoder = self.make_sparse(self.train)
+        ground_truth = self.val.explode()
+        return train_csr, user_encoder, item_encoder, ground_truth
---
+++ src/datasets/base_dataset.py
@@ -0,0 +1,132 @@
+import os
+import pickle
+from typing import Dict, Tuple, Union
+
+import pandas as pd
+
+DATASET_ROOT_PATH = "data"
+PROCESSED_DATASET_PATH = "processed.pickle"
+
+
+class BaseDataset:
+    
+    def __init__(self, args: dict) -> None: # rewrite to argparser
+        self.dataset_shortname = args["dataset_shortname"]
+        self.min_rating = args["min_rating"]
+        self.min_user_count = args["min_user_count"]
+        self.min_item_count = args["min_item_count"]
+
+        self.data_path = self._get_data_common_path()
+
+    def _load_raw_dataset(self) -> pd.DataFrame:
+        raw_data_path = os.path.join(self.data_path, "raw/ratings.csv")
+
+        raw_data = pd.read_csv(
+            raw_data_path,
+            header=0,
+            names=["user_id", "item_id", "rating", "timestamp"],
+        )
+
+        return raw_data
+
+    def _get_data_common_path(self) -> str:
+        path = os.path.join(DATASET_ROOT_PATH, self.dataset_shortname)
+        return path
+
+    def _filter_by_min_rating(self, interactions: pd.DataFrame) -> pd.DataFrame:
+        interactions = interactions[interactions.rating >= self.min_rating]
+        return interactions
+
+    def _get_count(self, interactions: pd.DataFrame, column_name: str) -> pd.Series:
+        count_groups = interactions[[column_name]].groupby(column_name)
+        count = count_groups.size()
+        return count
+
+    def _filter_triplets(self, interactions: pd.DataFrame) -> pd.DataFrame:
+        if self.min_user_count > 0:
+            user_count = self._get_count(interactions, "user_id")
+
+            interactions = interactions[
+                interactions["user_id"].isin(
+                    user_count.index[user_count >= self.min_user_count]
+                )
+            ]
+
+        if self.min_item_count > 0:
+            item_count = self._get_count(interactions, "item_id")
+
+            interactions = interactions[
+                interactions["item_id"].isin(
+                    item_count.index[item_count >= self.min_item_count]
+                )
+            ]
+
+        return interactions
+
+    def _encode_ids(
+        self, interactions: pd.DataFrame
+    ) -> Tuple[pd.DataFrame, dict, dict]:
+        user_mapping = {u: i for i, u in enumerate(interactions["user_id"].unique())}
+        item_mapping = {s: i for i, s in enumerate(interactions["item_id"].unique())}
+
+        interactions["user_id"] = interactions["user_id"].map(user_mapping)
+        interactions["item_id"] = interactions["item_id"].map(item_mapping)
+
+        return interactions, user_mapping, item_mapping
+
+    def _split(
+        self, interactions: pd.DataFrame, num_users: int
+    ) -> Tuple[dict, dict, dict]:
+
+        sorted_items = interactions.groupby("user_id").apply(
+            lambda d: list(d.sort_values(by="timestamp")["item_id"]),
+            include_groups=False,
+        )
+
+        train, val, test = {}, {}, {}
+        for user in range(num_users):
+            items = sorted_items[user]
+            train[user], val[user], test[user] = items[:-2], items[-2:-1], items[-1:]
+
+        return train, val, test
+
+    def _to_sparse_matrix(self, dataframe):
+        pass
+
+    def _filter(self) -> Dict[str, Union[pd.DataFrame, Dict[int, int]]]:
+        interactions = self._load_raw_dataset()
+
+        interactions = self._filter_by_min_rating(interactions)
+        interactions = self._filter_triplets(interactions)
+
+        interactions, user_mapping, item_mapping = self._encode_ids(interactions)
+
+        train, val, test = self._split(interactions, len(user_mapping))
+        dataset = {
+            "train": train,
+            "val": val,
+            "test": test,
+            "user_mapping": user_mapping,
+            "item_mapping": item_mapping,
+        }
+
+        return dataset
+
+    def preprocess(self) -> None:
+        processed_data_path = os.path.join(self.data_path, PROCESSED_DATASET_PATH)
+        if os.path.exists(processed_data_path):
+            raise FileExistsError()  # should use it?
+
+        preprocessed_data = self._filter()
+
+        with open(processed_data_path, "wb") as f:
+            pickle.dump(preprocessed_data, f)
+
+    def load_dataset(self) -> Dict[str, Union[pd.DataFrame, Dict[int, int]]]:
+        processed_data_path = os.path.join(self.data_path, PROCESSED_DATASET_PATH)
+
+        if not os.path.exists(processed_data_path):
+            self.preprocess()
+
+        interactions = pd.read_pickle(processed_data_path)
+        return interactions
